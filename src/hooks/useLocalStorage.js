49bfd084d2ce8a001d40a92d7b12e961951ddb90
Ben <ben.dalton@madetech.com>
2020-11-17 22:12:02 +0000

    add value state, return function value from local storage if exists, if not return value

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,22 @@
 import { useState, useEffect } from 'react';
 
-export default function useLocalStorage() {
+const PREFIX = 'codepen-clone0'
+
+export default function useLocalStorage(key, initialValue) {
+    const prefixedKey = PREFIX + key
+
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedKey)
+        if (jsonValue != null) return JSON.parse(jsonValue)
+        
+        if (typeof initialValue === 'function') {
+            return initialValue()
+        }
+        else {
+            return initialValue
+        }
+    });
+
     return (
         <div>
 
